494ba7f779e610f2c43015292a534eaf8c5259f3
anzaimasato <doogielaylak@gmail.com>
2016-11-16 01:27:57 -0500

    Add files via upload

---
+++ Assignment5/Homework5_MasatoAnzai.js
@@ -0,0 +1,48 @@
+//Masato Anzai
+//Midterm - Creative Coding
+//Creating a random zoo!
+//variables,loops,functions,colorMod(HSB)
+
+// color var init
+var r = 0;
+var g = 0;
+var b = 0;
+var scalar = random(windowWidth,windowHeight);
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  //calls randomSeed
+  clear();
+  bear(mouseX,mouseY,scalar)
+}
+
+//Randomizes colors
+function randomColor(){
+  r = int(random(0,255));
+  g = int(random(0,255));
+  b = int(random(0,255));
+}
+
+//Creates a bear
+function bear(x,y,s) {
+  push();
+  translate(x,y);
+  scale(s);
+  stroke(g);
+  strokeWeight(70);
+  line(0,-35,0,-35); //face
+  noStroke();
+  randomColor();
+  fill(r,g,b);
+  ellipse(-14,-35,8,8); //left eye
+  ellipse(14,-35,8,8); //right eye
+  randomColor();
+  fill(r,g,b);
+  triangle(4,-21,0,-15,-4,-21); //nose
+  ellipse(-25,-60,30,30); //left ear
+  ellipse(25,-60,30,30); //right ear
+  pop();
+}
